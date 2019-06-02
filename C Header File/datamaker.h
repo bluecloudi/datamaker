6619af0a6d0a4d5ce8f478a678397f7bc590fda9
开心果i <48399095+bluecloudi@users.noreply.github.com>
2019-06-02 18:30:41 +0800

    Update datamaker.h

--- C Header File/datamaker.h
+++ C Header File/datamaker.h
@@ -1,8 +1,9 @@
-#ifndef DATAMAKER_H
-#define DATAMAKER_H 1
+#ifndef ZSJ_H
+#define ZSJ_H 1
+#include<fstream>
 namespace std
 {
-    ifstream fin;
+	ifstream fin;
 	ofstream fout;
 	FILE *fr,*fw;
 	FILE *__cdecl fileo(char *name,char c)
@@ -10,6 +11,7 @@ namespace std
 		if(c=='r')
 		{
 			fin.open(name,ios::in);
+			
 			return fr=fopen(name,"r");
 		 } 
 		if(c=='w')
@@ -52,6 +54,62 @@ namespace std
 	int __cdecl fscan(const char *format,Args&... argument)
 	{
 		return fscanf(fr,format,argument...);
+	}
+	template<int n,int m,int wh,typename T>
+	struct graph{
+		struct edge1{
+			int u,v;
+			T w;
+		};
+		struct edge2{
+			int next,v;
+			T w;
+		};
+		#if wh==1
+		T e[n][n];
+		bool add(int u,int v,T w)
+		{
+			if(u>n||v>n||u<0||v<0) return false;
+			e[u][v]=w;
+			return true;
+		}
+		#endif
+		#if wh==2
+		edge2 e[m];
+		int head[n],p;
+		bool add(int u,int v,T w)
+		{
+			if(u>n||v>n||u<0||v<0) return false;
+			e[++p].v=v;
+			e[p].w=w;
+			e[p].next=head[u];
+			head[u]=p;
+			return true;
+		}
+		#endif
+		#if wh==3
+		edge1 e[m],p;
+		bool add(int u,int v,T w)
+		{
+			if(u>n||v>n||u<0||v<0) return false;
+			e[++p].u=u;
+			e[p].v=v;
+			e[p].w=w;
+			return true;
+		}
+		#endif
+	};
+	long long randll()
+	{
+		
+	}
+	long long rand_l_r()
+	{
+		
+	}
+	graph rand_graph(int n,int m,bool yw)
+	{
+		
 	}
 }
 #endif
